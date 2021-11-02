0b54e81c9ee50ebe6cbc38d425d2352b6060b3e6
yashsanghvi1005 <92793514+yashsanghvi1005@users.noreply.github.com>
2021-11-02 11:42:43 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Charusat</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 10px solid #00004d;
+  border-radius: 5px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+<body style="background-color:#b3e0ff ">
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p id="bj"> In 1994, Shri Charotar Moti Sattavis Patidar Kelavani Mandal was established from the parental organization (Matrusanstha) Shri Charotar Moti Sattavis Leuva Patidar Samaj, which has been working in the mission of social service through education for more than 125 years. The founder-president of the Kelavani mandal, the late Shri Chhotabhai Bhikhabhai Patel, and the late Dr. K.C. Patel (a renowned nuclear scientist and a prominent educationist of Gujarat), who succeeded him worked on the idea of a university at Education Campus, Changa (ECC). ECC started with the establishment of Charotar Institute of Technology - Changa (CITC) as the first institute which is now known as Chandubhai S. Patel Institute of Technology (CSPIT) in the year 2000.
+
+With the need for autonomy, the ECC applied for university status at UGC and Gujarat Government, which was granted on July 7, 2009, when the ECC was accorded the status of University and Charotar University of Science and Technology was established.</p>
+
+<p id="bj"></p>
+
+
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
